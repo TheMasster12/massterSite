37b4199f99c2ab78952ab5ef2d7fdbedbaa85d57
Andrew Mass <amass1212@gmail.com>
2013-07-31 15:46:28 -0500

    Some initial content for development

--- js/index.js
+++ js/index.js
@@ -3,26 +3,30 @@ function MassterSite() {}
 MassterSite.resize = function() {
   var height = $(window).height();
 
-  if($('.centered').height() >= height) {
-    $('.start').css('height','');
-    $('.start').css('min-height', height);
-  } else {
-      $('.start').css('min-height','');
-    if(height >= 500) {
-      $('.start').css('height', height);
+  if($('.start').css('display') != 'none') {
+    if($('.centered').height() >= height) {
+      $('.start').css('height','');
+      $('.start').css('min-height', height);
     } else {
-      $('.start').css('height', 500);
+        $('.start').css('min-height','');
+      if(height >= 500) {
+        $('.start').css('height', height);
+      } else {
+        $('.start').css('height', 500);
+      }
     }
-  }
-  
-  if(height >= 500) {
-    $('body').css('min-height', height);
-    $('.container.wrapper').css('min-height',height);
-    $('.container.wrapper:before').css('min-height', height);
-  } else {
-    $('body').css('min-height', 500);
-    $('.container.wrapper').css('min-height', 500);
-    $('.container.wrapper:before').css('min-height', 500);
+    
+    if(height >= 500) {
+      $('body').css('min-height', height);
+      $('.container.wrapper').css('min-height',height);
+      $('.container.wrapper:before').css('min-height', height);
+    } else {
+      $('body').css('min-height', 500);
+      $('.container.wrapper').css('min-height', 500);
+      $('.container.wrapper:before').css('min-height', 500);
+    }
+  } else if($('.content').css('display') != 'none') {
+      $('.sidebar-nav').css('height',height);
   }
 };
 
