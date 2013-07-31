6021912a375aed535e379b2918c51bb23746f60c
Andrew Mass <amass1212@gmail.com>
2013-07-31 13:03:48 -0500

    Layout maybe finished. Had to resort to javascript because css is really bad

--- js/index.js
+++ js/index.js
@@ -1,5 +1,35 @@
 function MassterSite() {}
 
-$(function() {
+MassterSite.resize = function() {
+  var height = $(window).height();
+
+  if($('.centered').height() >= height) {
+    $('.start').css('height','');
+    $('.start').css('min-height', height);
+  } else {
+      $('.start').css('min-height','');
+    if(height >= 500) {
+      $('.start').css('height', height);
+    } else {
+      $('.start').css('height', 500);
+    }
+  }
   
+  if(height >= 500) {
+    $('body').css('min-height', height);
+    $('.container.wrapper').css('min-height',height);
+    $('.container.wrapper:before').css('min-height', height);
+  } else {
+    $('body').css('min-height', 500);
+    $('.container.wrapper').css('min-height', 500);
+    $('.container.wrapper:before').css('min-height', 500);
+  }
+};
+
+$(function() {
+  MassterSite.resize();
+
+  $(window).resize(function() {
+    MassterSite.resize();
+  });  
 });
